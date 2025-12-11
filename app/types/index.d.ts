af4f3c75e5845f071e98cacde7a96eb41495f8ef
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2025-12-11 14:01:44 +0100

    update types

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,21 +1,28 @@
 // Book used in home page
 export type HomePageBook = {
-    id: number;
+    id: string;
     title: string;
     author?: string;
     genre?: {
+        id: string;
         name: string;
+        color: string;
     };
 };
 
 // List used in home page
 export type HomePageList = {
-    id: number;
+    id: string;
     name: string;
     books: HomePageBook[];
+    organization?: {
+        id: string;
+        name: string;
+    };
 };
 
 export type Genre = {
+    id: string;
     name: string;
     color: string;
     CreatedAt: string;
