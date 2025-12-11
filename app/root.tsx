079932aebd52b8480e28c75d9dc33d018abde80f
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2025-12-11 14:41:41 +0100

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <html lang="fr">
             <head>
                 <meta charSet="utf-8" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
                 <Links />
