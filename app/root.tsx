cd1ee31fd2e9a26caacf79d540bae217a9a6106f
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2026-01-19 12:16:04 +0100

    udpate max width

--- app/root.tsx
+++ app/root.tsx
@@ -45,7 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <Meta />
                 <Links />
             </head>
-            <body className="min-h-screen flex flex-col">
+            <body className="min-h-screen flex flex-col  min-w-[320px]">
                 {children}
                 <ScrollRestoration />
                 <Scripts />
