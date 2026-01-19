2bc7e1e699a93c2222b76a824fa9eab190b4059e
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2026-01-19 20:48:50 +0100

    add flash notifications for deleteBook

--- app/routes/book/deleteBook.tsx
+++ app/routes/book/deleteBook.tsx
@@ -6,33 +6,43 @@ import type { Route } from "./+types/deleteBook";
 import { deleteLinkBookList } from "~/db/booklist";
 import { removeBook } from "~/db/book";
 import { authMiddleware } from "~/middlewares/authMiddleware";
+import { commitSession, getSession } from "~/services/sessions.server";
 
 export const middleware: Route.MiddlewareFunction[] = [authMiddleware];
 
 export async function action({ params, request }: Route.ActionArgs) {
     const { supabase } = getSupabase(request);
+    const session = await getSession(request.headers.get("Cookie"));
 
     const { id: listId, bookId } = params;
 
     if (!bookId || !listId) {
+        /*
         throw new Response("Missing book id or list id", { status: 400 });
+        */
+        session.flash("error", "Identifiant manquant");
+        return redirect(`/`, {
+            headers: {
+                "Set-Cookie": await commitSession(session),
+            },
+        });
     }
 
-    //delete link between book and list
     try {
+        //delete link between book and list
         await deleteLinkBookList(supabase, { bookId, listId });
+        //delete book
+        await removeBook(supabase, { bookId });
+        //success message
+        session.flash("success", "Livre supprimé avec succès!");
     } catch (error) {
         console.error(error);
         return data({ errors: { form: "Erreur lors de la suppression de la liste" } }, { status: 500 });
     }
 
-    //delete book
-    try {
-        await removeBook(supabase, { bookId });
-    } catch (error) {
-        console.error(error);
-        return data({ errors: { form: "Erreur lors de la suppression du livre" } }, { status: 500 });
-    }
-
-    return redirect(`/list/${listId}`);
+    return redirect(`/list/${listId}`, {
+        headers: {
+            "Set-Cookie": await commitSession(session),
+        },
+    });
 }
